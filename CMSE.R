a82919329608d1d6482f476873ec559b4839665e
vlagani <vincenzo.lagani@gmail.com>
2018-07-31 09:41:22 +0200

    Updating CMSE with additional comments

--- CMSE.R
+++ CMSE.R
@@ -2,21 +2,27 @@
 library(pcalg)
 
 # Function for computing the causal mean squared error (CMSE) metric
+#
 # Input
 # originalGrap: an adjacency matrix representing the graph to be reconstructed
 # reconstructedGraph: an adjacency matrix representing the reconstructed graph
 # dataset: this dataset is generated from the original graph and 
-# it is used for building the reconstructed graph
+#          it is used for building the reconstructed graph
 # original.amat.type: the type of adjacency matrix for the original graph
-# See amat.type in ?pcalg::adjustment for further information
+#                     See amat.type in ?pcalg::adjustment for further information
 # reconstructed.amat.type: the type of adjacency matrix for the reconstructed graph
-# See amat.type in ?pcalg::adjustment for further information
+#                          See amat.type in ?pcalg::adjustment for further information
 # numVars: this is the number of randomly chosen nodes used in computing the cmse
-# if null (default), all nodes are used.
-# seed: the seed for the random number generator, useful for when numVars is specified
-# verbose: boolean, if true more information are provided
-# Output:
+#         if null (default), all nodes are used.
+# seed: the seed for the random number generator, useful when numVars is specified
+# verbose: boolean, if true information about the progress of the computation is displayed 
+# 
+# Output
 # cmse: the cmse value
+#
+# Authors: Vincenzo Lagani, Sofia Triantafillou, Konstanstinos Tsirlis
+#
+# Contact: vincenzo.lagani@iliauni.edu.ge
 
 CMSE <- function(originalGraph, reconstructedGraph, dataset, 
                          original.amat.type = 'pag', reconstructed.amat.type = 'pag',
@@ -94,25 +100,31 @@ causalEffect <- function(graph, dataset, amat.type, i, j){
   #checking if there is any adjustment set
   if(length(aSet) == 0){
     
-    #if no adjustment set, then no causal effect
+    #if there is no adjustment set, then we (arbitrarily) set the causal effect to zero
     EE <- 0
     
   }else{
     
-    #building the formula for the linear model
+    #building the formula for the linear model used to estimate the causal effect
     if(length(aSet[[1]]) > 0){
+      
+      #in this case there is at least one element in the adjustement set (other than node i)
       tmp <- logical(dim(graph)[1])
       tmp[aSet[[1]]] <- TRUE;
       varSet <- paste('V', aSet[[1]], sep = '')
       f <- as.formula(paste(paste0('V', j), '~', paste0('V', i), 
                             '+', paste(varSet, collapse = '+'))) 
+      
     }else{
+      
+      #no elements in the adjustement set, thus we only regress over node i
       f <- as.formula(paste(paste0('V', j), '~', paste0('V', i)))
+      
     }
     
     #fitting the linear model and extracting the coefficient
     linearModel <- lm(formula = f, data = dataset)
-    EE <- linearModel$coefficients[2]
+    EE <- linearModel$coefficients[2] #magic number derived by how the formula is constructed.
     
   }
   
